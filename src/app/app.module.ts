eb899fb4ffe7be6ee2a919575b0ab81f17a55ef8
Thanh Lam <62226747+ThanhLamm@users.noreply.github.com>
2021-09-25 18:36:08 +0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductComponent } from './components/product/product.component';
+import { RateComponent } from './components/rate/rate.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrderDetailComponent } from './components/order-detail/order-detail.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    SidebarComponent,
+    CategoryComponent,
+    ProductComponent,
+    RateComponent,
+    OrderComponent,
+    OrderDetailComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
