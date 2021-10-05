672023087e069f3171b371085b4eb643dd5e013c
ThanhLamm <lam03022001@gmail.com>
2021-10-05 10:37:24 +0700

    Update quản lý loại sản phẩm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -14,12 +18,20 @@ import { RateComponent } from './components/rate/rate.component';
 import { OrderComponent } from './components/order/order.component';
 import { OrderDetailComponent } from './components/order-detail/order-detail.component';
 import { CustomerComponent } from './components/customer/customer.component';
-import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { StatisticalYearComponent } from './components/statistical-year/statistical-year.component';
 import { StatisticalMonthComponent } from './components/statistical-month/statistical-month.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { EditCategoryComponent } from './components/edit-category/edit-category.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
@@ -50,14 +62,31 @@ const routes: Routes = [
     CustomerComponent,
     InventoryComponent,
     StatisticalYearComponent,
-    StatisticalMonthComponent
+    StatisticalMonthComponent,
+    AddCategoryComponent,
+    EditCategoryComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgbModule,
-    RouterModule.forRoot(routes, { enableTracing: true })
+    MatTableModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    ToastrModule.forRoot({
+      timeOut: 2500,
+      // progressBar: true,
+      progressAnimation: 'increasing',
+      // preventDuplicates: true,
+      closeButton: true,
+    }),
+    RouterModule.forRoot(routes, { enableTracing: true }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
+import { Category } from 'src/app/common/Category';
+import { CategoryService } from 'src/app/services/category.service';
 import { PageService } from 'src/app/services/page.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-category',
@@ -8,10 +15,61 @@ import { PageService } from 'src/app/services/page.service';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor(private pageService: PageService) { }
+  listData!: MatTableDataSource<Category>;
+  categories!: Category[];
+  categoriesLength!: number;
+  columns: string[] = ['categoryId', 'categoryName', 'view', 'delete'];
+  isLoading = true;
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private pageService: PageService, private toastr: ToastrService, private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.pageService.setPageActive('category');
+    this.getAll();
+  }
+
+  getAll() {
+    this.categoryService.getAll().subscribe(data => {
+      this.isLoading = false;
+      this.categories = data as Category[];
+      this.listData = new MatTableDataSource(this.categories);
+      this.listData.sort = this.sort;
+      this.listData.paginator = this.paginator;
+      this.categoriesLength = this.categories.length;
+    }, error => {
+      console.log('loi' + error);
+    })
+  }
+
+  finish() {
+    this.ngOnInit();
+  }
+
+  delete(id: number, name: string) {
+    Swal.fire({
+      title: 'Bạn muốn xoá nhãn hàng có tên ' + name + ' ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Xoá',
+      cancelButtonText: 'Không'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.categoryService.delete(id).subscribe(data => {
+          this.ngOnInit();
+          this.toastr.success('Thông báo xoá thành công!', 'Hệ thống');
+        }, error => {
+          this.toastr.error('Thông báo xoá thất bại, đã xảy ra lỗi!', 'Hệ thống');
+        })
+      }
+    })
+  }
+
+  search(event: any) {
+    const fValue = (event.target as HTMLInputElement).value;
+    this.listData.filter = fValue.trim().trim().toLowerCase();
   }
 
 }
